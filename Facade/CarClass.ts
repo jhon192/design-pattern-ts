e82aef3b79b1cd98c4b1deceaf2fac7540e6bc7b
jhon192 <jhon19.2@outlook.com>
2021-11-07 20:08:44 -0500

    upload

---
+++ Facade/CarClass.ts
@@ -0,0 +1,20 @@
+export class Ford {
+  constructor() {}
+  Operator() {
+    console.log("Soy un Ford");
+  }
+}
+
+export class Toyota {
+  constructor() {}
+  Operator() {
+    console.log("Soy un Toyota");
+  }
+}
+
+export class Honda {
+  constructor() {}
+  Operator() {
+    console.log("Soy un Honda");
+  }
+}
